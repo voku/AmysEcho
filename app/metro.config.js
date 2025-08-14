7c0a047158db87bee6b89984fc86992e6c8c8c59
Lars Moelleken <lars@moelleken.org>
2025-08-14 23:50:50 +0200

    chore: enable vision camera frame processor

--- app/metro.config.js
+++ app/metro.config.js
@@ -4,5 +4,13 @@ const config = getDefaultConfig(__dirname);
 
 // Include TFLite and MediaPipe Task model files in the bundle
 config.resolver.assetExts.push('tflite', 'task');
+// Register frame processor plugin if available
+let finalConfig = config;
+try {
+  const { withResizePlugin } = require('vision-camera-resize-plugin/metro');
+  finalConfig = withResizePlugin(config);
+} catch {
+  // Helper not present; proceed with default config
+}
 
-module.exports = config;
+module.exports = finalConfig;
