212204977fad8e8df7d76e389d67c603993d8aff
Lars Moelleken <lars@moelleken.org>
2025-08-27 12:24:19 +0200

    Package server tools for tests

--- server/test/test_normalize.py
+++ server/test/test_normalize.py
@@ -1,21 +1,16 @@
-import sys
-from pathlib import Path
-
 import numpy as np
 
-# make server/src importable
-sys.path.append(str(Path(__file__).resolve().parents[1] / "src"))
-from tools.train_mlp import _normalize  # noqa: E402
+from tools.train_mlp import _normalize
 
 
-def test_normalize_one_hand():
+def test_normalize_one_hand() -> None:
     left = [[float(i), 0.1, 0.1] for i in range(21)]
     res = _normalize(left)
     assert res is not None
     assert np.allclose(res[63:], 0)
 
 
-def test_normalize_two_hands():
+def test_normalize_two_hands() -> None:
     left = [[float(i), 0.1, 0.1] for i in range(21)]
     right = [[float(i), 0.1, 0.1] for i in range(21, 42)]
     res = _normalize(left + right)
