bcb3e4111a29dc1362f38f17d80552ebe99ed24f
Lars Moelleken <lars@moelleken.org>
2025-09-12 21:00:18 +0200

    chore: disable watch mode in sequential test runner

--- app/test/run-tests.js
+++ app/test/run-tests.js
@@ -6,7 +6,8 @@ const appDir = path.resolve(__dirname, '..');
 // Get list of test files from Jest
 const list = spawnSync('npx', ['jest', '--listTests'], {
   cwd: appDir,
-  encoding: 'utf8'
+  encoding: 'utf8',
+  env: { ...process.env, CI: '1' },
 });
 if (list.status !== 0) {
   console.error(list.stderr);
@@ -16,11 +17,22 @@ const files = list.stdout.trim().split('\n').filter(Boolean);
 
 for (const file of files) {
   console.log('Running', file);
-  const result = spawnSync(process.execPath, [require.resolve('jest/bin/jest'), '--runInBand', '--detectOpenHandles', '--verbose', file], {
-    cwd: appDir,
-    stdio: 'inherit',
-    env: { ...process.env, NODE_OPTIONS: '--max_old_space_size=8192' }
-  });
+  const result = spawnSync(
+    process.execPath,
+    [
+      require.resolve('jest/bin/jest'),
+      '--runInBand',
+      '--detectOpenHandles',
+      '--watchAll=false',
+      '--verbose',
+      file,
+    ],
+    {
+      cwd: appDir,
+      stdio: 'inherit',
+      env: { ...process.env, NODE_OPTIONS: '--max_old_space_size=8192', CI: '1' },
+    }
+  );
   if (result.status !== 0) {
     process.exit(result.status);
   }
