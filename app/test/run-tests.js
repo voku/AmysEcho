0e6d7e047a13fe4496fcce14fea980e868fe6a27
Lars Moelleken <lars@moelleken.org>
2025-09-12 19:42:09 +0200

    test: split app tests into individual runs

---
+++ app/test/run-tests.js
@@ -0,0 +1,29 @@
+const { spawnSync } = require('child_process');
+const path = require('path');
+
+const appDir = path.resolve(__dirname, '..');
+
+// Get list of test files from Jest
+const list = spawnSync('npx', ['jest', '--listTests'], {
+  cwd: appDir,
+  encoding: 'utf8'
+});
+if (list.status !== 0) {
+  console.error(list.stderr);
+  process.exit(list.status);
+}
+const files = list.stdout.trim().split('\n').filter(Boolean);
+
+for (const file of files) {
+  console.log('Running', file);
+  const result = spawnSync(process.execPath, [require.resolve('jest/bin/jest'), '--runInBand', '--detectOpenHandles', '--verbose', file], {
+    cwd: appDir,
+    stdio: 'inherit',
+    env: { ...process.env, NODE_OPTIONS: '--max_old_space_size=8192' }
+  });
+  if (result.status !== 0) {
+    process.exit(result.status);
+  }
+}
+
+console.log('All tests passed');
